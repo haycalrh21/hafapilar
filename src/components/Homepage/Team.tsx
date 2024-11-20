745b23cb48a678decc8dd9645efe5dcc42c59112
RAP <rap@Digitels-macbook.local>
2024-11-20 18:32:53 +0700

    team member

--- src/components/Homepage/Team.tsx
+++ src/components/Homepage/Team.tsx
@@ -6,8 +6,7 @@ export default function TeamCard() {
       name: "Hanni Utfa",
       position: "Founder",
       image: "/assets/team/HaeniUlfa.png",
-      university:
-        "B.S International Business Management, University of Oregon, USA",
+      university: "B.Sc International Business Management, University of Oregon, USA",
     },
     {
       name: "Frian Mandang",
@@ -24,20 +23,18 @@ export default function TeamCard() {
   ];
 
   return (
-    <div className=" mx-5 font-['Poppins']">
-      <h2 className="text-[24px] sm:text-[24px] lg:text-[50px] font-bold font-['Poppins'] text-center mb-4">
+    <div className="mx-5 font-['Poppins']">
+      <h2 className="text-[24px] sm:text-[24px] lg:text-[50px] font-bold text-center mb-4">
         Meet the Team
       </h2>
       <p className="text-center max-w-3xl mx-auto mb-8">
-        We bring a fresh, dynamic approach, uniting global recruitment
-        expertise, deep local market insight, and reliable support to ensure
-        success for international candidates.
+        We bring a fresh, dynamic approach, uniting global recruitment expertise, deep local market insight, and reliable support to ensure success for international candidates.
       </p>
-      <div className="flex flex-wrap justify-center gap-8">
+      <div className="flex flex-wrap justify-center w-full gap-20 sm:gap-20 lg:gap-40">
         {teamMembers.map((member, index) => (
           <div
             key={index}
-            className="w-full sm:w-[250px] bg-white rounded-2xl shadow-md overflow-hidden text-center  border-hijau border-[1px]"
+            className="w-full sm:w-[260px] lg:w-[250px] h-auto bg-white rounded-2xl shadow-md overflow-hidden text-center border-hijau border-[1px]"
           >
             <div className="relative w-full pt-[1%]">
               <img
